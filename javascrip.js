21f2202233ae600e34bee4189d5d1be397c7148e
ThzEdouard <edouard.flaquet@gmail.com>
2024-04-12 11:13:39 +0200

    edouard flaquet

---
+++ javascrip.js
@@ -0,0 +1,8 @@
+document.getElementById("ok").addEventListener('click', function()
+{
+    var name = document.getElementById("name").value;
+    console.log(name);
+    // var email = document.getElementById("email").innerText;
+    // var message = document.getElementById("message").innerText;
+    alert ("test");
+})
